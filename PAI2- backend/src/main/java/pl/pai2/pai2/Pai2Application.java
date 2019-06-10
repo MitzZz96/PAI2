5f38f618e31ae179478442fadea0058b320c9ab2
Adrian <mitzzz96@gmail.com>
2019-06-10 13:17:21 +0200

    first commit

---
+++ PAI2- backend/src/main/java/pl/pai2/pai2/Pai2Application.java
@@ -0,0 +1,13 @@
+package pl.pai2.pai2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pai2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pai2Application.class, args);
+	}
+
+}
---
+++ PAI2- backend/src/test/java/pl/pai2/pai2/Pai2ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.pai2.pai2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pai2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
